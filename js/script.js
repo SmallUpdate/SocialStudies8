c666329dc064130fbf942b01b185c2c6b4fef546
SmallUpdate <112966801+SmallUpdate@users.noreply.github.com>
2022-11-11 21:29:02 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,89 @@
+$(function() {
+
+  $(".chapter_1").click(function(){
+    $('.chapter_1_lessons').toggleClass("lessons_show")
+    $('.chapter_1').toggleClass("btn_active")
+  });
+
+  $(".chapter_2").click(function(){
+    $('.chapter_2_lessons').toggleClass("lessons_show")
+    $('.chapter_2').toggleClass("btn_active")
+  });
+
+  $(".chapter_3").click(function(){
+    $('.chapter_3_lessons').toggleClass("lessons_show")
+    $('.chapter_3').toggleClass("btn_active")
+  });
+
+  $(".chapter_4").click(function(){
+    $('.chapter_4_lessons').toggleClass("lessons_show")
+    $('.chapter_4').toggleClass("btn_active")
+  });
+});
+
+$(function() {
+  $("#first").click(function(){
+    $('#first').toggleClass("lesson_active")
+    $('#first_section').toggleClass("section_show")
+  });
+  
+  $("#second").click(function(){
+    $('#second').toggleClass("lesson_active")
+  });
+
+  $("#third").click(function(){
+    $('#third').toggleClass("lesson_active")
+  });
+  
+  $("#fourth").click(function(){
+    $('#fourth').toggleClass("lesson_active")
+  });
+  
+  $("#fifth").click(function(){
+    $('#fifth').toggleClass("lesson_active")
+  });
+  
+  $("#sixth").click(function(){
+    $('#sixth').toggleClass("lesson_active")
+  });
+
+  $("#seventh").click(function(){
+    $('#seventh').toggleClass("lesson_active")
+  });
+  
+  $("#eighth").click(function(){
+    $('#eighth').toggleClass("lesson_active")
+  });
+  
+  $("#ninth").click(function(){
+    $('#ninth').toggleClass("lesson_active")
+  });
+  
+  $("#tenth").click(function(){
+    $('#tenth').toggleClass("lesson_active")
+  });
+
+  $("#eleventh").click(function(){
+    $('#eleventh').toggleClass("lesson_active")
+  });
+  
+  $("#twelweth").click(function(){
+    $('#twelweth').toggleClass("lesson_active")
+  });
+  
+  $("#thirteenth").click(function(){
+    $('#thirteenth').toggleClass("lesson_active")
+  });
+  
+  $("#fourteenth").click(function(){
+    $('#fourteenth').toggleClass("lesson_active")
+  });
+
+  $("#fiveteenth").click(function(){
+    $('#fiveteenth').toggleClass("lesson_active")
+  });
+  
+  $("#sixteenth").click(function(){
+    $('#sixteenth').toggleClass("lesson_active")
+  });
+});
